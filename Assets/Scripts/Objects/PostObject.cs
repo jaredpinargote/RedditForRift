0b9aefcc90f586a9a5208cc369d019ea76debb38
Jared Pinargote <jaredpnrgt@yahoo.com>
2018-11-08 15:44:43 -0700

    working

---
+++ Assets/Scripts/Objects/PostObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+using Sparcopt.Reddit.Api.Domain;
+
+public class PostObject {
+	private Post post;
+	private Text text;
+	private GameObject gameObject;
+	public PostObject(Post p, Vector3 pos) {
+		gameObject = new GameObject();
+		gameObject.transform.position = pos;
+		post = p;
+		createPostTitleCanvas();
+	}
+
+	void createPostTitleCanvas() {
+		Canvas canvas = gameObject.AddComponent<Canvas>();
+		canvas.renderMode = RenderMode.WorldSpace;
+		CanvasScaler cs = gameObject.AddComponent<CanvasScaler>();
+		cs.referencePixelsPerUnit = 100f;
+		cs.dynamicPixelsPerUnit = 1f;
+		GraphicRaycaster gr = gameObject.AddComponent<GraphicRaycaster>();
+		gr.ignoreReversedGraphics = true;
+		RectTransform rt = gameObject.GetComponent<RectTransform>();
+		rt.position = gameObject.transform.position;
+		// rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0.002f);
+		// rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0.002f);
+		rt.sizeDelta = new Vector2(800f, 400f);
+		rt.localScale = new Vector3(0.002f, 0.002f, 0f);
+
+		// Text
+		Text t = gameObject.AddComponent<Text>();
+		t.alignment = TextAnchor.MiddleCenter;
+		t.horizontalOverflow = HorizontalWrapMode.Wrap;
+		t.verticalOverflow = VerticalWrapMode.Truncate;
+		Font ArialFont = (Font)Resources.GetBuiltinResource (typeof(Font), "Arial.ttf");
+		t.font = ArialFont;
+		t.fontSize = 300;
+		t.resizeTextForBestFit = true;
+		t.resizeTextMinSize = 1;
+		t.resizeTextMaxSize = 300;
+		t.text = post.Title;
+		t.enabled = true;
+		t.color = Color.white;
+
+		gameObject.name = "Post Title";
+	}
+}
---
+++ Assets/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sparcopt.Reddit.Api;
+
+public class ViewController : MonoBehaviour {
+	private List<PostObject> postObjects;
+
+	// Use this for initialization
+	void Start () {
+		loadSubredditPosts();
+	}
+
+	async void loadSubredditPosts() {
+		var redditService = new RedditService();
+		var subReddit = await redditService.GetSubredditAsync("all");
+		var position = new Vector3(0f, 0f, 0f);
+		foreach (var p in subReddit.Posts) {
+			var post = await redditService.GetPostAsync(p.Id);
+			PostObject postObject = new PostObject(post, position);
+			position.y += 0.82f;
+			// postObjects.Add(postObject);
+		}
+	}
+}
